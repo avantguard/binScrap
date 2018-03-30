3f64f274a18ab15883bef61da8e8cb47644dc7b4
avantguard <37925526+avantguard@users.noreply.github.com>
2018-03-30 14:37:47 +0200

    Update binscrap.py

--- binscrap.py
+++ binscrap.py
@@ -1,16 +1,16 @@
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
 import requests
 from time import sleep
 
 page = "https://support.binance.com/hc/en-us/sections/115000106672-New-Listings"
 query = requests.get(page)
-soup = BeautifulSoup(query.text)
+soup = BeautifulSoup(query.text, "html.parser")
 results = soup.findAll("li", {"class": "article-list-item "})
 
-print "==============================="
-print " Newly Added Coins on Binance: "
-print "==============================="
+print("===============================")
+print(" Newly Added Coins on Binance: ")
+print("===============================")
 sleep(1)
 
 for result in results:
-    print result.text
+    print(result.text)
